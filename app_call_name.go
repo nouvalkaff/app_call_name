ce7b6d57e2ee2cf22b2aa1321de93abc785b6dbe
M Nouval A Alkaff <abdelnouval@gmail.com>
2021-08-13 05:32:11 +0700

    add simple call name app v1.0.0

---
+++ app_call_name.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_call_name "github.com/nouvalkaff/mod_call_name"
+)
+
+func main() {
+	fmt.Println(go_call_name.Call_Name())
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nouvalkaff/app_call_name
+
+go 1.16
+
+require github.com/nouvalkaff/mod_call_name v1.0.0
