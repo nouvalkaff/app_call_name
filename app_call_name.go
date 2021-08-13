e3f78c4a77324f9047ac04a066b9d6d554f7199c
M Nouval A Alkaff <abdelnouval@gmail.com>
2021-08-13 09:46:49 +0700

    app with module version 2.5.1

--- app_call_name.go
+++ app_call_name.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 
-	go_call_name "github.com/nouvalkaff/mod_call_name"
+	go_call_name "github.com/nouvalkaff/mod_call_name/v2"
 )
 
+//go get github.com/nouvalkaff/mod_call_name/v2@v2.5.1
+// use go mod tidy to keep it works
+
 func main() {
-	fmt.Println(go_call_name.CallName("Mona", 22))
+	fmt.Println(go_call_name.CallName("Mona", 22, "Okay"))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/nouvalkaff/app_call_name
 
 go 1.16
 
-require github.com/nouvalkaff/mod_call_name v1.6.0
+require github.com/nouvalkaff/mod_call_name/v2 v2.5.1
